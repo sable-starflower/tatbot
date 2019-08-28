7402c9e72faf35bc928d4c0744fef0d6480dcb1e
Sable Starflower <sable@extropicstudios.com>
2019-08-28 14:44:37 -0700

    maek the tatbot
    nop

---
+++ tatbot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const client = new Discord.Client();
+
+var seen_words = {};
+fs.readFileSync(path.join(__dirname, './seen_words.txt'), {encoding: 'utf8'}).split("\n").forEach(function(phrase) {
+  seen_words[phrase] = true;
+});
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  // Ignore our own messages
+  if (msg.author.id == client.user.id) {
+    return;
+  }
+
+  var last_word = "";
+  msg.content.toUpperCase().replace(/[^0-9A-Z ]/g, '').split(" ").forEach(function(word) {
+
+    if (word.length != 4) {
+      last_word = "";
+      return;
+
+    }
+    if (last_word == "") {
+      last_word = word;
+    } else {
+      phrase = last_word + " " + word;
+      if (seen_words[phrase] != true) {
+        msg.reply(":punch: " + phrase + " :punch:");
+        seen_words[phrase] = true;
+        fs.writeFileSync(path.join(__dirname, './seen_words.txt'), Object.keys(seen_words).join("\n"), {encoding: 'utf8'});
+        last_word = "";
+      } else {
+        last_word = word;
+      }
+    }
+  });
+});
+
+client.login('token');
